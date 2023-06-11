eb31fee1ade962a26ec659a05f0134e955c4c56d
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-06-10 23:22:23 -0300

    first commit: composer create-project laravel/laravel sead. index rout and controller. show rout and controller.

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+use \App\Http\Controllers\MachinesController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [MachinesController::class, 'index']);
+
+Route::get('/listar-maquinas', [MachinesController::class, 'index']);
+
+Route::get('/visualizar-maquina/{id}', [MachinesController::class, 'show']);
+
