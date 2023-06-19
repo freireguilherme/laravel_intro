7d5492b397645cd8cce20bf330473863524a1704
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-06-19 18:07:19 -0300

    add: index imprime Id e Nome em tabela

--- resources/views/machines/index.blade.php
+++ resources/views/machines/index.blade.php
@@ -1,3 +1,23 @@
-<x-layout>
+<x-layout title='Listar as Máquinas'>
     <h1>Listar as Máquinas</h1>
+
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+            </tr>
+        </thead>
+
+        <tbody>
+
+            @foreach ($machines as $machine)
+                <tr>
+                    <td>{{$machine->id}}</td>
+                    <td>{{$machine->name}}</td>
+                </tr>
+            @endforeach
+        </tbody>
+
+    </table>
 </x-layout>
