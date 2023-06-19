d77df1218d799a030c275fba94e996e68d66667c
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-06-19 17:47:14 -0300

    add: recuperar dados pela Model

--- app/Http/Controllers/MachinesController.php
+++ app/Http/Controllers/MachinesController.php
@@ -2,11 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
+use App\Models\Machine;
+
 class MachinesController
 {
     public function index()
     {
         echo "<h1>Listar as máquinas</h1>";
+        //deve ser implementado em Models
+        //$machines = DB::select('SELECT id, name FROM machines');
+        //dd($machines); //dd é igual a varbump e para o processamento
+
+        
+        $machines = Machine::all();
+        dd($machines); //dd é igual a varbump e para o processamento
+
     }
 
     public function show($id)
