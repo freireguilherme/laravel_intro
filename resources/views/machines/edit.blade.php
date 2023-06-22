a362667dc085bf2da920c248298d5f1a223b95b5
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-06-22 12:29:24 -0300

    add: edit view

---
+++ resources/views/machines/edit.blade.php
@@ -0,0 +1,15 @@
+<x-layout title='Editar Máquinas'>
+
+    <a href="{{ route('machines.index') }}">Listar</a>
+
+    <h1>Editar Máquinas</h1>
+
+    <form action="{{ route('machines.update', $machine->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+        <label>Nome: </label>
+        <input type="text" name="name" id="name" placeholder="Nome da máquina"
+        value="{{ $machine->name }}"><br><br>
+        <button type="submit">Editar</button>
+    </form>
+</x-layout>
