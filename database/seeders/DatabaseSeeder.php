62ddf4ca0e9277627451c0950cb2dda78b9ba620
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-06-22 18:54:57 -0300

    add: Seeders e Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\MachinesSeeder;
+use App\Models\Machine;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call([
+            MachinesSeeder::class,
+        ]);
+
+        Machine::factory(count:100)->create();
     }
 }
